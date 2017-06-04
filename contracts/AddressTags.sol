98a829ae32857cc2b557a976c0cc00bbc2a9d95b
noah <baleen@protonmail.ch>
2017-06-04 17:24:57 -0500

    Some more stuff before uploading to github

--- contracts/AddressTags.sol
+++ contracts/AddressTags.sol
@@ -3,10 +3,13 @@ pragma solidity ^0.4.12;
 contract AddressTags {
     address private owner;
     mapping(address => Tag[]) tags;
+    indexEntry[] private invertedIndex;
+
 
     function AddressTags() {
         owner = msg.sender;
     }
+
 
     struct Tag {
         bytes32 tag;
@@ -14,16 +17,29 @@ contract AddressTags {
         address[] taggedBy;
     }
 
-    function addTag(address _address, bytes32 _tag)
+
+    struct indexEntry {
+        bytes32 tag;
+        matchAndWeight[] matches;
+    }
+
+
+    struct matchAndWeight {
+        address match;
+        uint weight;
+    }
+
+
+    function addTag(address _address, bytes32 _tag) returns (bool) {
         // Check if tag already exists, if so then add weight
         bool found = false;
         // Iterate through all tags
-        for (uint i = 0; i < tags[_address].length; i++) {
+        for (uint i = 0; i < tags[_address].length; i++)
             if (tags[_address][i].tag == _tag) {
                 // Check if sender address already added this tag
                 for (uint j = 0; j < tags[_address].taggedBy.length; j++) {
                     if (tags[_address][i].taggedBy[j] == msg.sender) {
-                        return; // or something
+                        return false; // or something
                     }
                 }
                 tags[_address][i].weight += 1;
@@ -39,6 +55,24 @@ contract AddressTags {
             newTag.weight = 1;
             newTag.taggedBy.push(msg.sender);
             tags[_address].push(newTag);
+        }
+        return true;
+    }
+
+
+    function getTags(address _address) returns Tag[] {
+        return tags[_address];
+    }
+
+
+    function private addToInvertedIndex(Tag _tag) returns (bool) {
+        return false;
+    }
+
+
+    function private incrementInvertedIndexWeight(Tag _tag) returns (bool) {
+        for (uint i = 0; i < invertedIndex.length; i++) {
+            return false;
         }
     }
 }
--- contracts/InvertedIndex.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.12;
-
-contract InvertedIndex {
-    address private owner;
-
-    function InvertedIndex() {
-        owner = msg.sender;
-    }
-}
