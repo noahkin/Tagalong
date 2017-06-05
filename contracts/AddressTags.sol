ffc531da8706372a212c24a046c0286c34f44a07
noah <baleen@protonmail.ch>
2017-06-04 19:26:21 -0500

    fixed some stuff

--- contracts/AddressTags.sol
+++ contracts/AddressTags.sol
@@ -1,9 +1,8 @@
-pragma solidity ^0.4.12;
+pragma solidity ^0.4.11;
 
 contract AddressTags {
     address private owner;
     mapping(address => Tag[]) tags;
-    indexEntry[] private invertedIndex;
 
 
     function AddressTags() {
@@ -18,26 +17,14 @@ contract AddressTags {
     }
 
 
-    struct indexEntry {
-        bytes32 tag;
-        matchAndWeight[] matches;
-    }
-
-
-    struct matchAndWeight {
-        address match;
-        uint weight;
-    }
-
-
     function addTag(address _address, bytes32 _tag) returns (bool) {
         // Check if tag already exists, if so then add weight
         bool found = false;
         // Iterate through all tags
-        for (uint i = 0; i < tags[_address].length; i++)
+        for (uint i = 0; i < tags[_address].length; i++) {
             if (tags[_address][i].tag == _tag) {
                 // Check if sender address already added this tag
-                for (uint j = 0; j < tags[_address].taggedBy.length; j++) {
+                for (uint j = 0; j < tags[_address][i].taggedBy.length; j++) {
                     if (tags[_address][i].taggedBy[j] == msg.sender) {
                         return false; // or something
                     }
@@ -53,26 +40,17 @@ contract AddressTags {
             Tag memory newTag;
             newTag.tag = _tag;
             newTag.weight = 1;
-            newTag.taggedBy.push(msg.sender);
+            //newTag.taggedBy.push(msg.sender);
             tags[_address].push(newTag);
         }
         return true;
     }
 
 
-    function getTags(address _address) returns Tag[] {
-        return tags[_address];
-    }
-
-
-    function private addToInvertedIndex(Tag _tag) returns (bool) {
-        return false;
-    }
-
-
-    function private incrementInvertedIndexWeight(Tag _tag) returns (bool) {
-        for (uint i = 0; i < invertedIndex.length; i++) {
-            return false;
+    function getTags(address _address) constant returns (bytes32[] tagValues, uint[] tagWeights) {
+        for (uint i = 0; i < tags[_address].length; i++) {
+            tagValues[i] = (tags[_address][i].tag);
+            tagWeights[i] = (tags[_address][i].weight);
         }
     }
 }
